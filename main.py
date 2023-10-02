651f124377bb8e0b64536de16710cdc7df3281e7
Barabama <barabama@qq.com>
2023-10-03 01:01:36 +0800

    Update

--- main.py
+++ main.py
@@ -1,31 +1,42 @@
 import re
 import requests
+import kuser_agent
 from bs4 import BeautifulSoup
 
 
-def get_url(url: str):
+def _get_url(url: str):
     """发送GET请求获取网页内容"""
-    response = requests.get(url)
+    headers = {"User-Agent": kuser_agent.get()}  # 设置请求头信息
+    response = requests.get(url, headers=headers)
     html_content = response.text
     return html_content
 
 
-def main():
-    url = "https://www.yudou66.com/search/label/free"
+def _scrape(url: str, element="", ret_all=True):
+    """获取网页元素"""
+    soup = BeautifulSoup(_get_url(url), "html.parser")
+    if element == "":
+        return soup
+    else:
+        return soup.find_all(element) if ret_all else soup.find(element)
+
+
+def scrape():
+    main_url = "https://www.yudou66.com/search/label/free"
 
     # 使用BeautifulSoup解析网页内容
-    soup = BeautifulSoup(get_url(url), "html.parser")
-    article = soup.find("article")
-    latest_link = article.find("a").get("href")
+    article = _scrape(main_url, "article", True)
+
+    article_url = article[2].find("a").get("href")
 
     # 使用正则表达式匹配链接字符串
     # pattern = r".+\.txt"
     pattern = r"http://yy\.yudou66\.top/[^/]+/[^/]+\.txt"
-    match = re.search(pattern, get_url(latest_link))
+    match = re.search(pattern, _get_url(article_url))
     if match:
         with open("FreeNodes.txt", "w") as f:
-            f.write(get_url(match.group()))
+            f.write(_get_url(match.group()))
 
 
 if __name__ == '__main__':
-    main()
+    scrape()
