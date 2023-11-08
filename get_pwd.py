4060bc0402d169eaa8ab2dd9a8d766248b1a6a83
Barabama <barabama@qq.com>
2023-11-08 23:02:54 +0800

    注释，调整结构，get_pwd改为迭代器

--- get_pwd.py
+++ get_pwd.py
@@ -15,17 +15,20 @@ def __get_frame(video_capture: cv2.VideoCapture) -> np.ndarray:
     :param video_capture: 视频流链接
     :return: 截图
     """
-    # 从几秒到几秒隔几秒截图
-    for i in range(70, 94, 2):
-        video_capture.set(cv2.CAP_PROP_POS_MSEC, i * 1000)
+    total_frames = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
+    fps = video_capture.get(cv2.CAP_PROP_FPS)
+    start_frame = total_frames * 2 // 3  # 从视频2/3处开始
+
+    for i in range(int(total_frames - fps), start_frame, int(-2 * fps)):
+        video_capture.set(cv2.CAP_PROP_POS_FRAMES, i)
         ret, frame = video_capture.read()
         if ret:
-            print(f"截图在 {i}s", end="\t")
+            print(f"截图在 {i // fps}s", end="\t")
             h, w = frame.shape[:2]
             yield frame[h // 6:h, w // 4:w * 3 // 4]  # 保留字幕部分
 
 
-def __is_pwd(text: str) -> str:
+def __find_pwd(text: str) -> str:
     # 判断文本中是否包含"密码"
     if "密码" in text:
         num_list = re.findall(r'\d+', text)
@@ -46,11 +49,11 @@ def _get_pwd_by_ocr(apicaller: APICaller, image: np.ndarray) -> str:
     words_result = result.get("words_result")
     num = result.get("words_result_num")
 
-    # 遍历截图中的候选词, 统计频率
+    # 遍历截图中的候选词
     for i in range(int(num)):
         text = words_result[i]["words"]
 
-        yield __is_pwd(text)
+        yield __find_pwd(text)
 
 
 def _get_pwd_from_caption(subtitles: list[pytube.Caption]) -> str:
@@ -71,15 +74,15 @@ def _get_pwd_from_caption(subtitles: list[pytube.Caption]) -> str:
             time = int(p_element.get("t"))
             duration = int(p_element.get("d"))
 
-            yield __is_pwd(text)
+            yield __find_pwd(text)
 
 
-def get_pwd(url: str, by_ocr: bool) -> list[tuple]:
+def get_pwd(url: str, by_ocr: bool) -> str:
     """
     获取候选密码列表
     :param url: YouTube 视频链接
     :param by_ocr: 是否通过 ocr
-    :return: 频率降序排列的元组列表
+    :return: 候选密码
     """
     # 创建 YouTube 对象
     yt = pytube.YouTube(url)
@@ -93,28 +96,23 @@ def get_pwd(url: str, by_ocr: bool) -> list[tuple]:
             print(f"获取视频流 {opt}")
             break
 
-    def generate_pwds():
-        if by_ocr:
-            # 调用ocr获得密码
-            apicaller = APICaller()  # apicaller 实例
-            cap = cv2.VideoCapture(stream.url)  # VideoCapture 实例获取截图
-            for frame in __get_frame(cap):
-                yield from (pwd for pwd in _get_pwd_by_ocr(apicaller, frame) if (pwd is not None) and len(pwd) > 2)
+    if by_ocr:
+        # 调用ocr获得密码
+        apicaller = APICaller()  # apicaller 实例
+        cap = cv2.VideoCapture(stream.url)  # VideoCapture 实例获取截图
+        for frame in __get_frame(cap):
+            yield from (pwd for pwd in _get_pwd_by_ocr(apicaller, frame) if pwd)
 
-            cap.release()  # 释放视频捕获对象
-        else:
-            # 通过字幕获取密码
-            subtitles = yt.captions.all()
-            yield from (pwd for pwd in _get_pwd_from_caption(subtitles) if (pwd is not None) and len(pwd) > 2)
-
-    pwd_generator = generate_pwds()
-    pwd_counter = collections.Counter(pwd_generator)
-    return pwd_counter.most_common()
+        cap.release()  # 释放视频捕获对象
+    else:
+        # 通过字幕获取密码
+        subtitles = yt.captions.all()
+        yield from (pwd for pwd in _get_pwd_from_caption(subtitles) if pwd)
 
 
 if __name__ == "__main__":
     # test
-    link_urls = [("https://youtu.be/ZUSNmlndeR8", False), ("https://youtu.be/y7Ccy0didgk", True)]
+    link_urls = [("https://youtu.be/ZUSNmlndeR8", False), ("https://youtu.be/bYEbmGSBsi8", True)]
 
-    pwds = get_pwd(*link_urls[1])
-    print(pwds)
+    for PWD in get_pwd(*link_urls[1]):
+        print(PWD)
