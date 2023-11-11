1b07328119f227522a21ffc23a505d0a2df0078b
Barabama <barabama@qq.com>
2023-11-11 17:55:19 +0800

    新建管理配置文件类

---
+++ Config.py
@@ -0,0 +1,38 @@
+import json
+from datetime import datetime
+from typing import TypedDict
+
+
+class ConfigData(TypedDict):
+    name: str
+    list_url: str
+    attrs: dict
+    pattern: str
+    by_ocr: bool
+    date: str
+
+
+class Config:
+    file_path: str
+    configs: list[ConfigData]
+
+    def __init__(self, file_path: str):
+        self.file_path = file_path
+        with open(file_path, "r") as file:
+            self.configs = json.load(file)
+
+    def get_configs(self):
+        return self.configs
+
+    def set_data(self, name: str, data:dict):
+
+        for config in self.configs:
+            if config["name"] == name:
+                config.update(data)
+                break
+            else:
+                print(f"未找到 {name}")
+
+    def write_config(self):
+        with open(self.file_path, "w") as file:
+            json.dump(self.configs, file, indent=4)
