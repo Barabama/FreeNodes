62597266b94f32b6658084a9b0dc438884c081b3
Barabama <barabama@qq.com>
2024-01-02 18:40:45 +0800

    节点信息处理

--- NodeHandler.py
+++ NodeHandler.py
@@ -21,34 +21,34 @@ def get_geo(add: str) -> str:
 def get_nodes(text: str) -> str:
     """遍历文本处理节点信息"""
     for node in text.strip().splitlines():
-        scheme, body = node.split("://")
-
-        if scheme == "vmess":
-            body_str = base64.b64decode(body).decode("utf-8")
-            node_dict = json.loads(body_str)
-            node_dict["ps"] = get_geo(node_dict["add"])
-            body_str = json.dumps(node_dict).encode("utf-8")
-            body = base64.b64encode(body_str).decode("utf-8")
-            node = f"{scheme}://{body}"
-
-        elif scheme == "ssr":
-            body_str = base64.b64decode(body).decode("utf-8")
-            remarks = get_geo(body_str.split(":")[0]).encode("utf-8")
-            remarks = base64.b64encode(remarks).decode("utf-8")
-            url = urlsplit(scheme + body_str)
-            params = parse_qs(url.query)
-            params.update({"remarks": [remarks]})
-            components = tuple([url.scheme, url.netloc, url.path,
-                                urlencode(params), url.fragment])
-            body_str = urlunsplit(components).encode("utf-8")
-            body = base64.b64encode(body_str).decode("utf-8")
-            node = f"{scheme}://{body}"
-
-        elif scheme in ("trojan", "ss", "vless"):
-            url = urlsplit(node)
-            add = url.netloc.split("@")[-1].split(":")[0]
-            fragment = get_geo(add)
-            components = tuple([url.scheme, url.netloc, url.path,
-                                url.query, quote(fragment)])
-            node = urlunsplit(components)
+        # scheme, body = node.split("://")
+        #
+        # if scheme == "vmess":
+        #     body_str = base64.b64decode(body).decode("utf-8")
+        #     node_dict = json.loads(body_str)
+        #     node_dict["ps"] = get_geo(node_dict["add"])
+        #     body_str = json.dumps(node_dict).encode("utf-8")
+        #     body = base64.b64encode(body_str).decode("utf-8")
+        #     node = f"{scheme}://{body}"
+        #
+        # elif scheme == "ssr":
+        #     body_str = base64.b64decode(body).decode("utf-8")
+        #     remarks = get_geo(body_str.split(":")[0]).encode("utf-8")
+        #     remarks = base64.b64encode(remarks).decode("utf-8")
+        #     url = urlsplit(scheme + body_str)
+        #     params = parse_qs(url.query)
+        #     params.update({"remarks": [remarks]})
+        #     components = tuple([url.scheme, url.netloc, url.path,
+        #                         urlencode(params), url.fragment])
+        #     body_str = urlunsplit(components).encode("utf-8")
+        #     body = base64.b64encode(body_str).decode("utf-8")
+        #     node = f"{scheme}://{body}"
+        #
+        # elif scheme in ("trojan", "ss", "vless"):
+        #     url = urlsplit(node)
+        #     add = url.netloc.split("@")[-1].split(":")[0]
+        #     fragment = get_geo(add)
+        #     components = tuple([url.scheme, url.netloc, url.path,
+        #                         url.query, quote(fragment)])
+        #     node = urlunsplit(components)
         yield node
