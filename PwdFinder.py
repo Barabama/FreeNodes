50676d30b1f583d2b10df1b247a135cd5333f326
Barabama <barabama@qq.com>
2024-02-05 23:42:24 +0800

    优化

--- PwdFinder.py
+++ PwdFinder.py
@@ -7,7 +7,6 @@ import cv2
 import numpy as np
 import pytube
 
-from MsgHandler import MsgHandler, CustomError
 from RequestHandler import OCRCaller
 
 
@@ -15,37 +14,35 @@ def find_pwd(text: str) -> str:
     """判断文本中是否包含密码"""
     if "码" in text:
         num_list = re.findall(r"\d+", text)
-        word = "".join(num_list)
-        return word
-    else:
-        return ""
+        return "".join(num_list)
+    else: return ""
 
 
 class PwdFinder:
     stream: pytube.Stream
     subtitles: list[pytube.Caption]
-
-    def __init__(self, msg_handler: MsgHandler, url: str, api_key: str, secret_key: str):
-        self.msg_handler = msg_handler
+    
+    def __init__(self, name: str, url: str, api_key: str, secret_key: str):
+        self.name = name
+        
         yt = pytube.YouTube(url)
         for opt in ["360p", "480p", "720p"]:
             if stream := yt.streams.get_by_resolution(opt):
-                self.msg_handler.show_msg(f"获取视频流 {opt}")
+                print(f"{self.name}: 获取视频流 {opt}")
                 self.stream = stream
                 break
-
+        
         self.subtitles = yt.captions.all()
-        if not self.subtitles:
-            self.ocr_caller = OCRCaller(api_key, secret_key)
-
+        if not self.subtitles: self.ocr_caller = OCRCaller(name, api_key, secret_key)
+    
     def gen_frame(self) -> Generator[np.ndarray, None, None]:
         """生成视频截图"""
         cap = cv2.VideoCapture(self.stream.url)
-
+        
         total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
         start_frame = total_frames * 1 // 3  # 从视频1/3处开始
         fps = cap.get(cv2.CAP_PROP_FPS)
-
+        
         # 密码靠后, 倒序截图
         for i in range(int(total_frames - fps), start_frame, int(-2 * fps)):
             cap.set(cv2.CAP_PROP_POS_FRAMES, i)
@@ -53,32 +50,34 @@ class PwdFinder:
             if ret:
                 h, w = frame.shape[:2]
                 yield frame[h * 3 // 4:h, w // 8:w * 7 // 8]  # 保留字幕部分
-
+        
         cap.release()  # 释放视频捕获对象
-
+    
     def gen_pwd(self) -> Generator[str, None, None]:
         """生成候选密码"""
         if self.subtitles:
             # 遍历字幕
             for caption in self.subtitles:
                 root = ET.fromstring(caption.xml_captions)
+                
                 # 遍历XML中的所有<p>元素
                 for p_elem in root.findall(".//p"):
                     yield find_pwd(p_elem.text)
+        
         else:
-            if not self.ocr_caller.access_token:
-                self.msg_handler.show_error(CustomError("OCRCaller初始化失败, 无法生成AccessToken"))
-                yield from []
-            self.msg_handler.show_msg("OCRCaller初始化成功, 生成AccessToken")
-
+            if self.ocr_caller.access_token:
+                print(f"{self.name}: OCRCaller初始化成功, 生成AccessToken")
+            else:
+                raise RuntimeError(f"{self.name}: OCRCaller初始化失败, 无法生成AccessToken")
+            
+            # 遍历视频截图
             for frame in self.gen_frame():
-                self.ocr_caller.img_to_base64(frame)  # 传入截图
-                result = self.ocr_caller.digital_ocr()  # 调用ocr
+                result = self.ocr_caller.request_ocr(frame)  # 传入截图请求ocr
                 words_result = result.get("words_result", [])
-                num = result.get("words_result_num", 0)
-
+                words_result_num = result.get("words_result_num", 0)
+                
                 # 遍历截图中的候选词
-                for i in range(num):
+                for i in range(words_result_num):
                     word = words_result[i].get("words", "")
                     yield find_pwd(word)
 
@@ -87,6 +86,5 @@ if __name__ == "__main__":
     # test
     link_urls = ["https://youtu.be/C7skrsccDQQ", "https://youtu.be/1lIctdmKqa0"]
     script, *args = sys.argv
-    msg_handler = MsgHandler("test")
-    pwd_finder = PwdFinder(msg_handler, link_urls[1], *args)
+    pwd_finder = PwdFinder("test", link_urls[1], *args)
     print([pwd for pwd in pwd_finder.gen_pwd() if pwd])
--- RequestHandler.py
+++ RequestHandler.py
@@ -1,4 +1,5 @@
 import base64
+from typing import TypedDict
 
 import cv2
 import kuser_agent
@@ -17,11 +18,29 @@ session.mount('http://', HTTPAdapter(max_retries=retries))
 session.mount('https://', HTTPAdapter(max_retries=retries))
 
 
-def make_request(method: str, url: str, params=None, data=None, headers=None, timeout=1):
+def make_request(method: str, url: str, params=None, data=None, headers=None, timeout=None):
     """请求服务器"""
     headers = headers if headers else {"User-Agent": kuser_agent.get()}
     response = session.request(method, url, params, data, headers, timeout=timeout)
     return response
+
+
+def img2base64(image: ndarray) -> base64:
+    """获取图像 base64 编码"""
+    retval, buffer = cv2.imencode(".jpg", image)
+    return base64.b64encode(buffer.tobytes())
+
+
+class WordsData(TypedDict):
+    words: str
+
+
+class OCRRes(TypedDict):
+    words_result: list[WordsData]
+    words_result_num: int
+    log_id: int
+    error_code: int
+    error_msg: str
 
 
 class OCRCaller:
@@ -32,36 +51,30 @@ class OCRCaller:
                  {"url": "https://aip.baidubce.com/rest/2.0/ocr/v1/webimage", "usable": True},
                  {"url": "https://aip.baidubce.com/rest/2.0/ocr/v1/accurate", "usable": True}]
     access_token: str
-    image: ndarray
-    b_base64: base64
-
-    def __init__(self, api_key: str, secret_key: str):
+    name: str
+    
+    def __init__(self, name: str, api_key: str, secret_key: str):
         """使用 AK, SK 生成鉴权签名(Access Token)"""
-        params = {"grant_type": "client_credentials", "client_id": api_key, "client_secret": secret_key}
+        params = {"grant_type": "client_credentials",
+                  "client_id" : api_key, "client_secret": secret_key}
         response = make_request("POST", self.access_url, params=params)
         self.access_token = response.json().get("access_token")
-
-    def img_to_base64(self, image: ndarray):
-        """获取图像 base64 编码"""
-        self.image = image
-        retval, buffer = cv2.imencode(".jpg", image)
-        self.b_base64 = base64.b64encode(buffer.tobytes())
-
-    def digital_ocr(self) -> dict:
-        """
-        向百度云数字ocr发送图片, 接收响应
-        :return: {"words_result":[] , "words_result_num": int, "log_id"}
-        """
+        self.name = name
+    
+    def request_ocr(self, image: ndarray) -> OCRRes:
+        """向百度云数字ocr发送图片, 接收响应"""
         params = {"access_token": self.access_token}
-        headers = {"Content-Type": "application/x-www-form-urlencoded", "Accept": "application/json"}
-        payload = {"image": self.b_base64, "detect_direction": "false"}
-
+        headers = {"Content-Type": "application/x-www-form-urlencoded",
+                   "Accept"      : "application/json"}
+        payload = {"image": img2base64(image), "detect_direction": "false"}
+        
         # 发送 POST 请求
         for i, item in enumerate(self.post_urls):
             if not item["usable"]: continue
             response = make_request("POST", item.get("url"), params, payload, headers)
-            res = response.json()
-            if "error_code" in res:
+            result = OCRRes(**response.json())
+            if "error_code" in result:
+                print(result["error_msg"])
                 self.post_urls[i]["usable"] = False
                 continue
-            return res if res else {}
+            else: return result
