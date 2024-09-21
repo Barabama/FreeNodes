a04d3c95b3d09b90dd8e03a6758ac59971b99cba
Barabama <barabama@qq.com>
2024-09-21 12:50:06 +0800

    Optimize

--- utils/PwdFinder.py
+++ utils/PwdFinder.py
@@ -15,12 +15,6 @@ from paddleocr import PaddleOCR
 from skimage.metrics import structural_similarity as ssim
 
 
-def find_password(text: str, key: str) -> str:
-    if key in text:
-        nums = re.findall(r"\d+", text)
-        return "".join(nums)
-
-
 def _keyframe_iter(url: str, threshold=0.8) -> Generator[tuple[int, np.ndarray], None, None]:
     """Generate keyframes backwards."""
     cap = cv2.VideoCapture(url)
@@ -38,7 +32,7 @@ def _keyframe_iter(url: str, threshold=0.8) -> Generator[tuple[int, np.ndarray],
         if not ret:
             break
 
-        frame = frame[height * 3 // 4:height, 0:width]
+        # frame = frame[height * 3 // 4:height, 0:width]
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         if prev is not None and ssim(prev, gray) < threshold:
             yield i, frame
@@ -52,7 +46,7 @@ class PwdFinder:
     name: str
     logger: logging.Logger
     date: str
-    description: str
+    descriptions: list[str]
     subtitles: pytubefix.CaptionQuery
     stream: pytubefix.Stream
     ocr: PaddleOCR
@@ -61,12 +55,13 @@ class PwdFinder:
         self.name = name
         self.logger = logger
 
-        yt = pytubefix.YouTube(url, use_oauth=True, allow_oauth_cache=True,
+        yt = pytubefix.YouTube(url, use_oauth=True, use_po_token=True,
+                               allow_oauth_cache=True,
                                on_progress_callback=on_progress)
 
         # date = yt.publish_date.date() # Not working
         # self.date = date.strftime("%Y-%m-%d")
-        print(type(yt))
+
         match = re.search(r"(?:\d{4}[-年])?(\d{1,2})[-月](\d{1,2})", yt.title)
         if not match:
             self.logger.error(f"{name} found no date")
@@ -75,7 +70,7 @@ class PwdFinder:
         self.date = date.strftime("%Y-%m-%d")
         self.logger.info(f"{name} found date: {self.date}")
 
-        self.description = yt.description
+        self.descriptions = yt.description.splitlines()
         self.subtitles = yt.captions
         if not self.subtitles:
             self.logger.warning(f"{name} found no subtitles")
@@ -113,16 +108,20 @@ class PwdFinder:
     def password_iter(self, key: str):
         """Generate possible passwords."""
         text_iter = self._xml_caption_iter if self.subtitles else self._ocr_result_iter
-        for text in itertools.chain(iter(self.description), text_iter()):
-            if pwd := find_password(text, key):
-                yield pwd
+        for text in itertools.chain(iter(self.descriptions), text_iter()):
+            self.logger.debug(f"{self.name} found text: {text}")
+            if key not in text:
+                continue
+            yield from filter(lambda x: x, re.findall(f"\\d+", text))
 
 
 if __name__ == "__main__":
     logging.disable(logging.DEBUG)
-    urls = ['https://youtu.be/H5tMkb1SpEo']
+    urls = ['https://www.youtube.com/watch?v=StNJHi5b2oM',
+            'https://www.youtube.com/watch?v=is7vn5URVcc']
     logger = logging.getLogger()
     console = logging.StreamHandler()
     console.setLevel(logging.DEBUG)
     logger.addHandler(console)
-    f = PwdFinder('test', logger, urls[0])
+    for url in urls:
+        f = PwdFinder('test', logger, url)
